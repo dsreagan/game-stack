df108be9ef8b91ca7f7484e30b0b873d3a3d063e
Daniel Reagan <d.reagan.dr@gmail.com>
2023-08-13 14:06:00 -0500

    Add genre display

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,23 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react"
 import useGenres from "../hooks/useGenres.js"
+import getCroppedImageUrl from "../sevices/image-url.js"
 
 export default function GenreList() {
   const { data } = useGenres()
   return (
-    <ul>
-      {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY='5px'>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize='lg'>{genre.name}</Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
   )
 }
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,8 +3,9 @@ import useData from "./useData.js"
 export interface Genre {
   id: number
   name: string
+  image_background: string
 }
 
-const useGenres = () => useData<Genre>('/genres')
+const useGenres = () => useData<Genre>("/genres")
 
 export default useGenres
