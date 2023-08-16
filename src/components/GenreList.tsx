176c7b5b1388c8fefd431b03b52875799a42b8b1
Daniel Reagan <d.reagan.dr@gmail.com>
2023-08-16 13:03:55 -0500

    Customize theme for darker grays

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -26,7 +26,7 @@ export default function GenreList({ onSelectGenre, selectedGenre }: Props) {
     <>
       <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
       <List>
-        {data.map((genre) => (
+        {data.map((genre: Genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
